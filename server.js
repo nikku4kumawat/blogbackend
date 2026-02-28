8fb6da73761de7254653cf05348a2f5e8f09a9db
nikku4kumawat <nikkukumawat4@gmail.com>
2026-02-28 16:53:38 +0530

    Initial commit

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/", (req, res) => {
 });
 
 // port
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log("Server started on port " + PORT);
